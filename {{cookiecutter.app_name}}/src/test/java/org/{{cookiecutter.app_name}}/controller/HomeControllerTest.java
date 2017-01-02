4eb21fd0ae8779a3e46182b02f1cd7bcb02c515c
martykells <martykells@gmail.com>
2017-01-02 22:39:52 +0000

    test folder structure tidy up

---
+++ {{cookiecutter.app_name}}/src/test/java/org/{{cookiecutter.app_name}}/controller/HomeControllerTest.java
@@ -0,0 +1,14 @@
+package org.{{cookiecutter.app_name}}.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        HomeController homeController = new HomeController();
+        assertEquals("Hello world", homeController.home());
+    }
+}
